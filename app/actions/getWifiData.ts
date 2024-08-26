23758f40511334e37a95babaffdbf78c2d4031df
JUN <gurwns232@naver.com>
2024-08-26 16:08:52 +0900

    feat: getwifidata 에러 처리

--- app/actions/getWifiData.ts
+++ app/actions/getWifiData.ts
@@ -4,16 +4,20 @@ import axios from "axios";
 const getWifiData = async (
   searchParams: URLSearchParams,
 ): Promise<WifiData> => {
-  const queryParams = new URLSearchParams(searchParams.toString());
+  try {
+    const queryParams = new URLSearchParams(searchParams.toString());
 
-  const response = await axios.get(`/api/wifi`, {
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-    params: queryParams,
-  });
+    const response = await axios.get(`/api/wifi`, {
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      params: queryParams,
+    });
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    throw new Error("An unexpected error occurred. Please try again.");
+  }
 };
 
 export default getWifiData;
